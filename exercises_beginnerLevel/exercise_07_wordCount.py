43bfa7cef1415dc9fea9739b6369c5c0bb6e5b99
Juna Teres Martin <junateresmartin@gmail.com>
2023-03-02 23:59:18 +0530

    Sentence Word Count

---
+++ exercises_beginnerLevel/exercise_07_wordCount.py
@@ -0,0 +1,8 @@
+#AUTHOR   :  JUNA TERES MARTIN
+#PROGRAM  :  A program that finds out the number of words in a string
+#DATE     :  02-03-2023
+
+string=input("Enter a string: ")
+string2=string.split()
+count=len(string2)
+print(f"Number of words in the string = {count}")
