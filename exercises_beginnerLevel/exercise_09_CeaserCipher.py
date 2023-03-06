3e744661bf55b77b5d31252f5199babe0096ee69
Juna Teres Martin <junateresmartin@gmail.com>
2023-03-06 23:37:32 +0530

    aa

---
+++ exercises_beginnerLevel/exercise_09_CeaserCipher.py
@@ -0,0 +1,24 @@
+#AUTHOR   :  JUNA TERES MARTIN
+#PROGRAM  :  A program to do ceaser cipher of a string to encode the message
+#DATE     :  06-03-2023
+
+string=input("Enter a string: ")
+cipher_string=""
+distance=int(input("Enter a distance: "))
+for i in string:
+    asci=ord(i)
+    if asci>=ord("a") and asci<=ord("z"):
+        cipher_value=asci+distance
+        if cipher_value>ord("z"):
+            cipher_value=(cipher_value-ord("z"))+(ord("a")-1)
+            cipher_string+=chr(cipher_value)
+        else:
+            cipher_string+=chr(cipher_value)
+    elif asci>=ord("A") and asci<=ord("Z"):
+        cipher_value=asci+distance
+        if cipher_value>ord("Z"):
+            cipher_value=(cipher_value-ord("Z"))+(ord("A")-1)
+            cipher_string+=chr(cipher_value)
+        else:
+            cipher_string+=chr(cipher_value)
+print(cipher_string)
