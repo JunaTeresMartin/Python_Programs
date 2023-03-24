c2132e45abea450baec37d94a3cd1d05a96a4cfe
Juna Teres Martin <junateresmartin@gmail.com>
2023-03-24 21:43:44 +0530

    syntax error remode

--- exercises_beginnerLevel/exercise_01.py
+++ exercises_beginnerLevel/exercise_01.py
@@ -2,8 +2,8 @@
 two_digit_number = input("Type a two digit number: ")
 print(type(two_digit_number))
 two_digit_number=int(two_digit_number)#type casting
-num=two_digit_number/10;
-rem=two_digit_number//10;
+num=two_digit_number/10
+rem=two_digit_number//10
 print(num+rem)
 
 #or
