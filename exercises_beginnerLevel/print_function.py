b6ad10c12fb81972239cffe4b05c91a2e7cb9f14
Juna Teres Martin <junateresmartin@gmail.com>
2023-03-24 21:38:45 +0530

    added escape sequences

--- exercises_beginnerLevel/print_function.py
+++ exercises_beginnerLevel/print_function.py
@@ -1,4 +1,27 @@
+#AUTHOR   :  JUNA TERES MARTIN
+#PROGRAM  :  Basic things in Python :)
+#LAST DATE MODIFIED  :  24-03-2023
+
+
 print("Hello World!")
 print("Hello World!","Howdy???")
 print("Hello World! ","Howdy???")
 print("A","B","C","D")
+print("A",end="")
+print("B","C","D")
+print("Jackie"+"Chan")
+print("Jackie "+"Chan")
+print("Jackie","Chan")
+print(8)
+print(8 * 2)
+print(8 ** 2)
+print(8/12)
+print(8 // 12)
+#print(8/0)  ZeroDivisionError: division by zero
+print('Good\tday')
+print('Good\nday')
+print('Good\bnight')
+print('good\\day')
+print('good\'day')
+print('good\"day')
+print('g'+'o'*8 +"d day")
