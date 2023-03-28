efc760b1ffc63fd3b862a2cfb0bb13ae26080f67
Juna Teres Martin <junateresmartin@gmail.com>
2023-03-28 22:41:36 +0530

    28/03/2023

---
+++ exercises_beginnerLevel/exercise_28_dictionaryToListAndMore.py
@@ -0,0 +1,12 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = list(car.keys())
+y = list(car.values())
+z = list(car.items())
+print("\n",x)# ['brand', 'model', 'year']
+print("\n",y)# ['Ford', 'Mustang', 1964]
+print("\n",z)# [('brand', 'Ford'), ('model', 'Mustang'), ('year', 1964)]
