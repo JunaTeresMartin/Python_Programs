828560a2e5759f0de6985bd5d064ccd295bf6919
JunaTeresMartin <83876430+JunaTeresMartin@users.noreply.github.com>
2023-04-25 15:22:12 +0530

    Create km_to_m.py

---
+++ exercises_intermediate/oop/km_to_m.py
@@ -0,0 +1,9 @@
+class DistanceConversion:
+    def get_distance(self):
+        self.distanecInKm=float(input("Enter the distance in km: "))
+    def print_distance(self):
+        distanecInM=self.distanecInKm*1000
+        print(distanecInM)
+d=DistanceConversion()
+d.get_distance()
+d.print_distance()
