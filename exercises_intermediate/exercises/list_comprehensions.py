1426c251a33253167b6ba04951a61821dada741a
Juna Teres Martin <junateresmartin@gmail.com>
2023-05-13 20:56:58 +0530

    13/05/2023

---
+++ exercises_intermediate/exercises/list_comprehensions.py
@@ -0,0 +1,6 @@
+'''
+List comprehension in Python is a concise way of creating --> lists <-- from the ones that already exist. 
+It provides a shorter syntax to create new lists from existing lists and their values.
+'''
+squares=[n**2 for n in range(1,51)]
+print(squares)
