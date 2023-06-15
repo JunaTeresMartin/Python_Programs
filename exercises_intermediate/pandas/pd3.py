fa5f414cfe9daa523f7189e2b824042eee561efd
JunaTeresMartin <83876430+JunaTeresMartin@users.noreply.github.com>
2023-06-15 15:08:37 +0530

    Update pd3.py

--- exercises_intermediate/pandas/pd3.py
+++ exercises_intermediate/pandas/pd3.py
@@ -3,3 +3,19 @@ df=pd.read_csv("details.csv")
 print(df)
 df=pd.read_csv("details.csv",index_col="roll no")
 print(df)
+#gives a description
+print(df.describe())
+#gives the number of columns and rows
+print(df.shape)
+#sum of marks
+print(df["marks"].sum())
+#minimum value in marks
+print(df["marks"].min())
+#max
+print(df["marks"].max())
+#varience
+print(df["marks"].var())
+#mean
+print(df["marks"].mean())
+#std deviation
+print(df["marks"].std())
